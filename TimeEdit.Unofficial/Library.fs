84119ca1d5fb3a4946e685836ff1675b47b604d5
Joel Linder <joelsc.linder@gmail.com>
2024-04-17 20:23:39 +0200

    Git init

---
+++ TimeEdit.Unofficial/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace TimeEdit.Unofficial
+
+open FSharp.Data;
+
+module TimeEdit =
+    let private BASE_URL = "https://cloud.timeedit.net/lu/web/lth1/";
+    
+    let queryProgram name =
+        let queryUrl = $"%s{BASE_URL}objects.html?partajax=t&sid=1002&search_text=%s{name}&types=191"
+        
+        let queryPage = HtmlDocument.Load(queryUrl)
+        let programObj = queryPage.Elements() |> Seq.filter (fun elem -> elem.AttributeValue("data-name") = name) |> Seq.head
+        
+        programObj.AttributeValue("data-idonly")
+        
+        
